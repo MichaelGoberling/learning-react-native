b86142d3ec7d8b34980f6d859323c59b8cbe1618
Michael Goberling <Michael.Goberling@gmail.com>
2019-12-14 16:43:12 -0600

    Add other goal managemnt functions and goal adding modal

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -13,25 +13,49 @@ import GoalInput from "./components/GoalInput";
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalhandler = goalTitle => {
     setCourseGoals(courseGoals => [
       ...courseGoals,
       { id: Math.random().toString(), value: goalTitle }
     ]);
+    setIsAddMode(false);
   };
 
-  const clearGoalsHandler = () => {
-    setCourseGoals([]);
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.id !== goalId);
+    });
+  };
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
   };
 
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalhandler} />
+      <Button
+        title="Add New Goal"
+        onPress={() => {
+          setIsAddMode(true);
+        }}
+      />
+      <GoalInput
+        visible={isAddMode}
+        onAddGoal={addGoalhandler}
+        onCancel={cancelGoalAdditionHandler}
+      />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
-        renderItem={itemData => <GoalItem title={itemData.item.value} />}
+        renderItem={itemData => (
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />
+        )}
       ></FlatList>
     </View>
   );
