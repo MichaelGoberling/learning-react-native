e995b3a041785f22e583d5c57284efcc3701f468
Michael Goberling <Michael.Goberling@gmail.com>
2019-12-15 23:16:57 -0600

    Initial Commit: Guess a Number project

---
+++ rn-guess-a-number/screens/StartGameScreen.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+
+const StartGameScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a New Game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a Number!</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center"
+  },
+  tile: {
+    fontSize: 20,
+    marginVertical: 10
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center"
+  },
+  buttonContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 15
+  }
+});
+
+export default StartGameScreen;
