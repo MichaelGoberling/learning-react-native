14ea42b9ff7a7cf632af6e54cf84e54b29f347da
Michael Goberling <Michael.Goberling@gmail.com>
2019-12-16 23:27:13 -0600

    Guess a Number: Add round tracking, handle game over and restart states

--- rn-guess-a-number/App.js
+++ rn-guess-a-number/App.js
@@ -1,26 +1,47 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import Header from "./components/Header";
-import StartGameScreen from "./screens/StartGameScreen";
-import GameScreen from "./screens/GameScreen";
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState();
 
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
+    setUserNumber(null);
+  };
+
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
+  };
+
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-  if (userNumber) {
-    content = <GameScreen userChoice={userNumber} />;
+  if (userNumber && guessRounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    content = (
+      <GameOverScreen
+        userNumber={userNumber}
+        roundsNumber={guessRounds}
+        onRestart={configureNewGameHandler}
+      />
+    );
   }
 
   return (
     <View style={styles.screen}>
-      <Header title="Guess a Number" />
+      <Header title='Guess a Number' />
       {content}
     </View>
   );
--- rn-guess-a-number/screens/GameScreen.js
+++ rn-guess-a-number/screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
@@ -20,8 +20,17 @@ const GameScreen = props => {
     generateRandomBetween(1, 100, props.userChoice)
   );
 
+  const [currentRounds, setCurrentRounds] = useState(0);
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
+
+  const { userChoice, onGameOver } = props;
+
+  useEffect(() => {
+    if (currentGuess === userChoice) {
+      onGameOver(currentRounds);
+    }
+  }, [currentGuess, userChoice, onGameOver]);
 
   const nextGuessHandler = direction => {
     if (
@@ -46,6 +55,7 @@ const GameScreen = props => {
       currentGuess
     );
     setCurrentGuess(nextNumber);
+    setCurrentRounds(currentRounds => currentRounds + 1);
   };
 
   return (
