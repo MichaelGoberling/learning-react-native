123431005b992bb13e1f58debd03c071c2311089
Michael Goberling <Michael.Goberling@gmail.com>
2019-12-16 23:42:26 -0600

    Guess a Number: Add fonts and app loading managemnt for loading fonts

--- rn-guess-a-number/App.js
+++ rn-guess-a-number/App.js
@@ -1,14 +1,36 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { AppLoading } from 'expo';
+
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
 
+import * as Font from 'expo-font';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState();
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinished={() => setDataLoaded(true)}
+        onError{(err) => console.log(err)}
+      />
+    );
+  }
 
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
