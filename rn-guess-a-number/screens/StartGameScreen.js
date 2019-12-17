935d0e8d01ad142c1a1399eef3903f31f15ecea9
Michael Goberling <Michael.Goberling@gmail.com>
2019-12-16 23:05:27 -0600

    Guess a Number: Add number advancing logic, resuable styled number container, and dummy game over screen

--- rn-guess-a-number/screens/StartGameScreen.js
+++ rn-guess-a-number/screens/StartGameScreen.js
@@ -13,6 +13,7 @@ import {
 import Card from "../components/Card";
 import Colors from "../constants/colors";
 import Input from "../components/Input";
+import NumberContainer from "../components/NumberContainer";
 
 const StartGameScreen = props => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -43,12 +44,22 @@ const StartGameScreen = props => {
     setConfirmed(true);
     setEnteredValue("");
     setSelectedNumber(parseInt(enteredValue));
+    Keyboard.dismiss();
   };
 
   let confirmedOutput;
 
   if (confirmed) {
-    confirmedOutput = <Text>Chosen number: {selectedNumber}</Text>;
+    confirmedOutput = (
+      <Card style={styles.summaryContainer}>
+        <Text>You selected</Text>
+        <NumberContainer>{selectedNumber}</NumberContainer>
+        <Button
+          title="START GAME"
+          onPress={() => props.onStartGame(selectedNumber)}
+        />
+      </Card>
+    );
   }
 
   return (
@@ -121,6 +132,10 @@ const styles = StyleSheet.create({
   input: {
     width: 50,
     textAlign: "center"
+  },
+  summaryContainer: {
+    marginTop: 20,
+    alignItems: "center"
   }
 });
 
