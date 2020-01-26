1e742a6c7ab2dc1a659ca639eeb09b255ae99d39
Michael Goberling <Michael.Goberling@gmail.com>
2020-01-25 21:06:58 -0600

    Meals App: Add primitive navigation to meals detail screen

--- rn-meals-app/screens/CategoryMealsScreen.js
+++ rn-meals-app/screens/CategoryMealsScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 const CategoryMealsScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>CategoryMealsScreen works!</Text>
+      <Button
+        title='Go to meals detail screen!'
+        onPress={() => {
+          props.navigation.navigate({ routeName: 'MealDetail' });
+        }}
+      />
     </View>
   );
 };
